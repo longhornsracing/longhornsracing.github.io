47cc0854c7d07ac09f60ad9c90fa7bf04a114ae2
KryophoraX <balaskanda.s@gmail.com>
2025-11-25 16:50:51 -0500

    Initialize project with Babel, ESLint, and basic HTML structure

---
+++ index.js
@@ -0,0 +1,6 @@
+// JavaScript entry point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hero from './Components/racing/Hero';
+
+ReactDOM.render(<Hero />, document.getElementById('root'));
