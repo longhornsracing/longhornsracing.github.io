2b7acfe0a784c1d94250338a76fa72c6da4fc37f
KryophoraX <balaskanda.s@gmail.com>
2025-11-25 17:46:24 -0500

    Deploy: publish production build (bundle.js) to GitHub Pages

---
+++ bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+var _Hero = _interopRequireDefault(require("./Components/racing/Hero"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+// JavaScript entry point
+
+_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement(_Hero["default"], null), document.getElementById('root'));
--- index.js
+++
@@ -1,6 +0,0 @@
-// JavaScript entry point
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hero from './Components/racing/Hero';
-
-ReactDOM.render(<Hero />, document.getElementById('root'));
